79800db9ce661747520a7d01157861097e4a9a56
BoatTK <jmartinobst@gmail.com>
2025-05-19 21:28:20 -0400

    Added Icon, Cleanup

--- src/main/java/net/boattk/ancientmaps/AncientCityMaps.java
+++ src/main/java/net/boattk/ancientmaps/AncientCityMaps.java
@@ -3,8 +3,14 @@ package net.boattk.ancientmaps;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.block.MapColor;
+import net.minecraft.item.map.MapDecorationType;
 import net.minecraft.item.map.MapDecorationTypes;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.StructureTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
@@ -16,22 +22,19 @@ import org.slf4j.LoggerFactory;
 
 public class AncientCityMaps implements ModInitializer {
 	public static final String MOD_ID = "ancient-city-maps";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		RegistryKey<MapDecorationType> registryKey = RegistryKey.of(RegistryKeys.MAP_DECORATION_TYPE, Identifier.ofVanilla("ancient_city"));
+		MapDecorationType mapDecorationType = new MapDecorationType(Identifier.ofVanilla("ancient_city"), true, 3827290, true, false);
+		RegistryEntry<MapDecorationType> ANCIENT_CITY =  Registry.registerReference(Registries.MAP_DECORATION_TYPE, registryKey, mapDecorationType);
 
-		TradeOfferHelper.registerVillagerOffers(VillagerProfession.CARTOGRAPHER, 1, factories -> {
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.CARTOGRAPHER, 5, factories -> {
 			factories.add(
-						new TradeOffers.SellMapFactory(14, (TagKey<Structure>)TagKey.of(RegistryKeys.STRUCTURE, Identifier.ofVanilla("on_ancient_city_maps")), "filled_map.mansion", MapDecorationTypes.MANSION, 12, 10)
+						new TradeOffers.SellMapFactory(16, (TagKey<Structure>)TagKey.of(RegistryKeys.STRUCTURE, Identifier.ofVanilla("on_ancient_city_maps")), "filled_map.ancient_city", ANCIENT_CITY, 12, 10)
 			);
 		});
+		LOGGER.info("Ancient City Maps Successfully Loaded");
 	}
 }
